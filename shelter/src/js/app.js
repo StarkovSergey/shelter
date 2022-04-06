f54c4d9365322b13275db973e4fe39131b31ee19
Starkov Sergey <starkovsr@gmail.com>
2022-04-06 13:56:57 +0300

    feat: prepare files for first deploy

--- shelter/src/js/app.js
+++
@@ -1,1 +0,0 @@
-import './modules/toggle.js';
--- shelter/src/js/modules/toggle.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.querySelector('.navigation__toggle');
-const navigationElement = document.querySelector('.navigation');
-
-toggleButton.addEventListener('click', () => {
-  navigationElement.classList.toggle('navigation--open');
-});
